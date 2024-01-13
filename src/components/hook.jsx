42d16b61fe08d2ae6e0f05562494ab205260d923
James-wasonga <jameswasonga22@gmail.com>
2024-01-13 12:37:02 -0800

    first commits

--- src/components/hook.jsx
+++ src/components/hook.jsx
@@ -6,13 +6,5 @@ function handleClick() {
     setCount(count+1);
 }
 
-return (
-    <>
-
-    <button count={count} onClick={handleClick}/>
-    <button count={count} onClick={handleClick}/>
-
-    </>
-)
 
 export default mybtn
