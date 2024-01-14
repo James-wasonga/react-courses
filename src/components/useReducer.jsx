25a9b7795fdaafeaf61d0f8445aa735f27103aa2
James-wasonga <jameswasonga22@gmail.com>
2024-01-14 04:33:10 -0800

    update commits

--- src/components/useReducer.jsx
+++ src/components/useReducer.jsx
@@ -9,6 +9,12 @@ const reducer = (state, action) => {
         const [state, dispatch] = useReducer(reducer,{count: 0,showText})
 
     }
-    
+    return (
+        <div>
+            <h1>{state.count}</h1>
+            <button  onClick={() =>{}} >click here </button>
+            {state.showText && <p>This is a text</p>}   
+        </div>
+    )
 }
-export default reducer;
+export default reducer
