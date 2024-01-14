9717ec984c80e4fd62a4e99d3f923f2d464b5cd6
James-wasonga <jameswasonga22@gmail.com>
2024-01-14 04:37:08 -0800

    update commits

--- src/components/useReducer.jsx
+++ src/components/useReducer.jsx
@@ -4,7 +4,12 @@ const reducer = (state, action) => {
     switch(action.type) {
         case "INCREMENT":
             return{count: state.count+1, showText: state.showText};
+            case "toggleShowText":
+            return {count: state.count, showText: !state.showText};
+            default:
+                return state;
     }
+
     const ReducerTutorial = () => {
         const [state, dispatch] = useReducer(reducer,{count: 0,showText})
 
@@ -16,5 +21,5 @@ const reducer = (state, action) => {
             {state.showText && <p>This is a text</p>}   
         </div>
     )
-}
+    }
 export default reducer
