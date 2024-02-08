6d12879490c012ee90ae416fbeeaa627dbfc9fe7
James-wasonga <jameswasonga22@gmail.com>
2024-02-08 22:57:21 +0300

    update commit

--- src/components/EscapeBack.jsx
+++ src/components/EscapeBack.jsx
@@ -8,7 +8,14 @@ function Profile() {
     return (
         <>
         <h1>{user.name}</h1>
-     
+        <img className="avatar"
+        src = {user.imageUrl}
+        alt= {'photo of '+ user.name}
+        style = {{
+            width:user.imageSize,
+            height:user.imageSize
+        }}
+        />
         </>
     )
 }
